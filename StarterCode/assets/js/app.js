f18a7eeb7fca4cdb343e177818dd59f60bef1d13
cmtaflin <cmtaflin@gmail.com>
2018-12-26 12:09:12 -0500

    space for chart, no chart, but no errors in console

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -24,68 +24,71 @@ var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Step 4: Data Load Function and unpack data object
-d3.csv("assets/data/data.csv",then(function(err, data){
-    if(err) throw err;
+function dataSetup(){
+    d3.csv("assets/data/data.csv"),then(function(error, data){
+        if(error) throw error;
 
-    data.forEach(function(hData){
-        hData.id= +hData.id;
-        hData.state= +hData.state;
-        hData.abbr= +hData.abbr;
-        hData.poverty= +hData.poverty;
-        hData.povertyMoe = +hData.povertyMoe;
-        hData.age = +hData.age;
-        hData.ageMoe = +hData.ageMoe;
-        hData.income = +hData.income;
-        hData.incomeMoe = +hData.incomeMoe;
-        hData.healthcare = +hData.healthcare;
-        hData.healthcareLow = +hData.healthcareLow;
-        hData.healthcareHigh = +hData.healthcareHigh;
-        hData.obesity = +hData.obesity;
-        hData.obesityLow = +hData.obesityLow;
-        hData.obesityHigh = +hData.obesityHigh;
-        hData.smokes = +hData.smokes;
-        hData.smokesLow = +hData.smokesLow;
-        hData.smokesHigh = +hData.smokesHigh;
-    });
+        data.forEach(function(hData){
+            hData.id= +hData.id;
+            hData.state= +hData.state;
+            hData.abbr= +hData.abbr;
+            hData.poverty= +hData.poverty;
+            hData.povertyMoe = +hData.povertyMoe;
+            hData.age = +hData.age;
+            hData.ageMoe = +hData.ageMoe;
+            hData.income = +hData.income;
+            hData.incomeMoe = +hData.incomeMoe;
+            hData.healthcare = +hData.healthcare;
+            hData.healthcareLow = +hData.healthcareLow;
+            hData.healthcareHigh = +hData.healthcareHigh;
+            hData.obesity = +hData.obesity;
+            hData.obesityLow = +hData.obesityLow;
+            hData.obesityHigh = +hData.obesityHigh;
+            hData.smokes = +hData.smokes;
+            hData.smokesLow = +hData.smokesLow;
+            hData.smokesHigh = +hData.smokesHigh;
+        });
+    });    
 
 
-    // Step 5: Build Scales
-    var xScale = d3.xScale()
-        .domain(d3.extend(hData,d=> d.poverty))
-        .range([0,width]);
+        // Step 5: Build Scales
+        var xScale = d3.xScale()
+            .domain(d3.extend(hData,d=> d.poverty))
+            .range([0,width]);
 
-    var yScale = d3.yScale()
-        .domain(d3.extend(hData,d=> d.healthcare))
-        .range([height,0]);
+        var yScale = d3.yScale()
+            .domain(d3.extend(hData,d=> d.healthcare))
+            .range([height,0]);
 
-    // Step 6: Create Axis Fuctions with Axis Titles
-    var bottomAxis= d3.axisBottom(xScale);
-    var leftAxis = d3.axisLeft(yScale);
+        // Step 6: Create Axis Fuctions with Axis Titles
+        var bottomAxis= d3.axisBottom(xScale);
+        var leftAxis = d3.axisLeft(yScale);
 
-    chartGroup.append("g")
-        .attr("transform",'translate(0, ${height})')
-        .call(bottomAxis);
+        chartGroup.append("g")
+            .attr("transform",'translate(0, ${height})')
+            .call(bottomAxis);
 
-    chartGroup.append("g")
-        .attr("transfrom",'translate (${width},0')
-        .call(leftAxis);
+        chartGroup.append("g")
+            .attr("transfrom",'translate (${width},0')
+            .call(leftAxis);
 
-    chartGroup.append("text")
+        chartGroup.append("text")
+            .attr("transform",'translate(${width/2},${height + margin.top + 20})')
+            .classed("x-axis text",true)
+            .text("In Poverty %");
+        
+        chartGroup.append("text")
         .attr("transform",'translate(${width/2},${height + margin.top + 20})')
-        .classed("x-axis text",true)
-        .text("In Poverty %");
-    
-    chartGroup.append("text")
-    .attr("transform",'translate(${width/2},${height + margin.top + 20})')
-    .classed("y-axis text",true)
-    .text("Lack Healthcare %");
+        .classed("y-axis text",true)
+        .text("Lack Healthcare %");
 
-    // var circleGroup = chartGroup.selectAll("circle")
-    //     .data(hdata)
-    //     .enter()
-    //     .append("circle")
-    //     .attr("cx", (d, i)=> xScale(i))
-    //     .attr("cy", (d,i)=> yScale(d))
-    //     .attr("r","5")
-    //     .attr("fill","red"); 
-}));
+        // var circleGroup = chartGroup.selectAll("circle")
+        //     .data(hdata)
+        //     .enter()
+        //     .append("circle")
+        //     .attr("cx", (d, i)=> xScale(i))
+        //     .attr("cy", (d,i)=> yScale(d))
+        //     .attr("r","5")
+        //     .attr("fill","red"); 
+    
+};
